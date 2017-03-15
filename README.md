35ab935c7dc8c8e9c0d5bac4cafc513117937b63
Yu Ishihara <ishihara.yu@gmail.com>
2017-03-16 00:47:53 +0900

    add files

---
+++ README.md
@@ -0,0 +1,13 @@
+# Gulp(Sass + PostCSS Autoprefixer + Minify) + Bootstrap4
+
+server:
+
+```
+$ gulp
+```
+
+watch only:
+
+```
+$ gulp watch
+```
---
+++ tasks/watch.js
@@ -0,0 +1,85 @@
+'use strict';
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var sass = require('gulp-ruby-sass');
+var connect = require('gulp-connect');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var plumber = require("gulp-plumber");
+var sourcemaps = require('gulp-sourcemaps');
+var minifycss = require('gulp-minify-css');
+var postcss = require('gulp-postcss');
+var filter = require("gulp-filter");
+var notify = require('gulp-notify');
+
+var paths = {
+  dist: './public',
+  scripts: [
+    'assets/javascripts/**/*.js'
+    ],
+  styles: [
+    'assets/stylesheets/**/*.scss',
+    'assets/stylesheets/**/*.sass'
+    ]
+};
+
+gulp.task('connect', function() {
+  connect.server({
+    root: paths.dist
+  });
+});
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: paths.dist
+    }
+  });
+});
+
+gulp.task('scripts', function() {
+  gulp.src(paths.scripts)
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./public/javascripts'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('styles', function() {
+  var processors = [
+    require('autoprefixer')({ browsers: ['last 3 version', 'ie 8'], cascade: false })
+  ];
+
+  gulp.src(paths.styles)
+    .pipe(plumber())
+    .pipe(sass())
+    .on('error', function(err) {
+      console.log(err.message);
+    })
+    .pipe(minifycss())
+    .pipe(filter("**/*.css"))
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('./public/stylesheets'))
+    .pipe(reload({stream:true}));
+});
+
+gulp.task('watch_reload', function() {
+  gulp.run('connect');
+  gulp.run('browserSync');
+
+  gulp.watch([paths.scripts], ['scripts']);
+  gulp.watch([paths.styles], ['styles']);
+});
+
+gulp.task('watch', function() {
+  gulp.watch([paths.scripts], ['scripts']);
+  gulp.watch([paths.styles], ['styles']);
+});
+
+gulp.task('default', function () {
+  gulp.start('watch_reload');
+});
